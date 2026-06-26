ea1bc91c0ea3d7a3ed4468e399e8997cdb2c8540
franifu04-coder <franifu04@gmail.com>
2026-06-26 06:55:40 -0300

    Instrucciones (escena 1)

---
+++ Assets/Scripts/Instrucciones.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instrucciones : MonoBehaviour
+{
+    public float waitTime = 3f;
+    public float fadeTime = 2f;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        float timer = 0f;
+        Color originalColor = spriteRenderer.color;
+
+        while (timer < fadeTime)
+        {
+            timer += Time.deltaTime;
+
+            float alpha = Mathf.SmoothStep(1f, 0f, timer / fadeTime);
+
+            spriteRenderer.color = new Color(
+                originalColor.r,
+                originalColor.g,
+                originalColor.b,
+                alpha
+            );
+
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+}
